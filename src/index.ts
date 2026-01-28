3623c6e308303bbf9b8f3b64c34a795b47240151
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-01-28 23:23:14 +0900

    feat: export 추가

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { DataGSMProvider, useDataGSMConfig } from './lib/store/OauthProvider';
+export { DataGSMLoginButton } from './lib/components/button';
+export { useDataGSM } from './lib/hooks/useDataGSM';
+export type { DataGSMConfig } from './lib/types/types';
